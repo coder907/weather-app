8c75322959ea99011096d1e831f5b6f895af5e50
coder907 <coder907@example.com>
2019-02-06 15:29:45 +0100

    Updates

---
+++ precache-manifest.1462d87d98b2d489ab7a49d9cbedbaa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "18a530d9d9fdee17c028",
+    "url": "/weather-app/static/js/main.18a530d9.chunk.js"
+  },
+  {
+    "revision": "eadbd472f9ac09fb5ea9",
+    "url": "/weather-app/static/js/1.eadbd472.chunk.js"
+  },
+  {
+    "revision": "18a530d9d9fdee17c028",
+    "url": "/weather-app/static/css/main.1e73ebbf.chunk.css"
+  },
+  {
+    "revision": "039de396940cace9454081fe1bfa11e1",
+    "url": "/weather-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.e6e6a379ad54ed65882f5b2e42e06e7d.js"
+  "/weather-app/precache-manifest.1462d87d98b2d489ab7a49d9cbedbaa6.js"
 );
 
 workbox.clientsClaim();
